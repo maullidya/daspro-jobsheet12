a8c7c64a0795767eb37bf7442df805bc98b1e38a
maullidya <zhramldy@gmail.com>
2025-11-27 12:11:22 +0700

    modifikasi soal percobaan 2 nomor 6

--- kafe15.java
+++ kafe15.java
@@ -1,5 +1,17 @@
-public class kafe15{    
-    public static void main(String[] args) {
+public class kafe15{
+    public static void Menu(String namaPelangan, boolean isMember, String kodePromo) {
+     System.out.println("Selamat datang, "+namaPelangan+"!");
+     
+     if (isMember) {
+        System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+     }
+    if (kodePromo.equals("DISKON50")) {
+        System.out.println("Anda mendapatkan diskon 50%");
+    } else if (kodePromo.equals("DISKON30")) {
+        System.out.println("Anda mendapatkan diskon 30%");
+    } else{
+        System.out.println("invalid.");
+    }
      System.out.println("==== MENU RESTO KAFE ====");
      System.out.println("1. Kopi Hitam - Rp 15,000");
      System.out.println("2. Cappuccino - Rp 20,000");
@@ -7,5 +19,9 @@ public class kafe15{
      System.out.println("4. Teh Tarik - Rp 12,000");
      System.out.println("5. Roti Bakar - Rp 10,000");
      System.out.println("6. Mie Goreng - Rp 18,000");
-     System.out.println("Silakan pilih menu yang Anda inginkan");    }
+     System.out.println("Silakan pilih menu yang Anda inginkan");
+    }
+    public static void main(String[] args) {
+        Menu("Andi", true,"");
+    }
 }
