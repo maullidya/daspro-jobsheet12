68fe5ae558929ed0e677bcf3ed54d40e6dd07b24
maullidya <zhramldy@gmail.com>
2025-11-27 17:56:53 +0700

    tugas 1

---
+++ Kubus15.java
@@ -0,0 +1,23 @@
+public class Kubus15 {
+    public static int volumeKubus(int sisi) {
+        int volumeKubus = sisi * sisi * sisi;
+        return volumeKubus;
+    }
+
+
+    public static int luasPermukaanKubus(int sisi) {
+        int luasPermukaanKubus = 6 * sisi * sisi;
+        return luasPermukaanKubus;
+    }
+
+    public static void main(String[] args) {
+        int sisi = 3;
+
+        int volume = volumeKubus(sisi);
+        int luas = luasPermukaanKubus(sisi);
+
+        System.out.println("Sisi kubus : " + sisi);
+        System.out.println("Volume kubus : " + volume);
+        System.out.println("Luas permukaan kubus : " + luas);
+    }
+}
